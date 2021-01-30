a37839b2b9d62193085a07c341558f31d66839f5
magicwind-yyd <77655874+magicwind-yyd@users.noreply.github.com>
2021-01-30 21:20:37 +0800

    c files
    add the code

---
+++ main.c
@@ -0,0 +1,8 @@
+#include "fun.h"
+int main() {
+	
+    //mciSendString("open 1.mp3 alias start", NULL, 0, NULL);// 打开音频文件
+    //mciSendString("play start repeat", NULL, 0, NULL);// 循环播放
+    mainmenu();
+return 0;
+}
